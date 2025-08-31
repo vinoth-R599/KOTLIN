9b5cd12d1805665a9d3e94ced4e477ce33f88c67
vinoth-R599 <vinothramesh599@gmail.com>
2025-08-31 16:38:58 +0530

    Create kotlin if_else condition3.kt

---
+++ kotlin if_else condition3.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val trafficLightColor = "Green"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+
+    } else {
+        println("Go")
+    }
+}
